21b73088ad27dd4ae637a55b01376a194e300a1c
Seongjin <seongjinkim1123@gmail.com>
2020-02-28 23:56:07 +0900

    Edit Programmers-Quiz2

--- 1_Programmers/Quiz2/Seong/solution.js
+++ 1_Programmers/Quiz2/Seong/solution.js
@@ -56,27 +56,3 @@ return primeCnt;
 
 
 
-
-
-// Set
-
-function solution(n) {
-    const s = new Set();
-    for(let i=1; i<=n; i+=2){
-        s.add(i);
-    }
-    s.delete(1);
-    s.add(2);
-    for(let j=3; j<Math.sqrt(n); j++){
-        if(s.has(j)){
-             for(let k=j*2; k<=n; k+=j){    
-                s.delete(k);
-             }
-        }
-    }
-    return s.size;
-}
-
-
-
-
