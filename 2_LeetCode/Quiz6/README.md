9e160deae3988a2fa6efa66d735466e7f40190fc
Seongjin <seongjinkim1123@gmail.com>
2020-03-08 22:49:28 +0900

    Code LeetCode Easy 242

---
+++ 2_LeetCode/Quiz6/README.md
@@ -0,0 +1,6 @@
+## 242 - Valid Anagram 문제
+  : input string 이 주어진 string 의 Anagram 인지 확인 (철자 순서만 바꾼 단어)
+
+- ### Solution
+  1. #### 나의 코드  [>>](solution.js)
+  2. #### 참고 코드 (더 효율적): [>>](others.js)
---
+++ 2_LeetCode/Quiz6/others.js
@@ -0,0 +1,6 @@
+function isAnagram(s, t) {
+	const map = {};
+	s.split('').map(c => map[c] = map[c] ? map[c] + 1 : 1);
+	t.split('').map(c => map[c] = map[c] ? map[c] - 1 : -1);
+	return Object.keys(map).every(k => map[k] === 0);
+}
