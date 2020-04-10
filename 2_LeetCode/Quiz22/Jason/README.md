195518649c8a93af9e36e1503aaad6bf01451964
Seongjin <seongjinkim1123@gmail.com>
2020-04-11 00:09:01 +0900

    Code LeetCode Easy 141

---
+++ 2_LeetCode/Quiz22/Jason/README.md
@@ -0,0 +1,16 @@
+## 141. Linked List Cycle
+
+:
+
+- O(1) 으로 풀기
+
+- ### Solution
+
+  1. #### 나의 코드 [>>](solution).js)
+
+```
+Memo
+
+나의 코드 풀이 :
+
+```
---
+++ 2_LeetCode/Quiz22/Jason/solution.js
@@ -0,0 +1,15 @@
+const hasCycle = (head) => {
+  let p1 = head;
+  let p2 = head;
+
+  while (p2 && p2.next && p2.next.next) {
+    p1 = p1.next;
+    p2 = p2.next.next;
+
+    if (p1 === p2) {
+      return true;
+    }
+  }
+
+  return false;
+};
